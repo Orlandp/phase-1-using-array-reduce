f5f93c8d22aa1f72f71439c13636f7846b032c53
pknowledge <jamesblssings@gmail.com>
2024-10-04 06:54:47 +0300

    how to reduce an array

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Assume batteryBatches is already defined as an array of battery counts
+
+// Use reduce to sum all the battery counts
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+console.log(totalBatteries);  // Output: 55
